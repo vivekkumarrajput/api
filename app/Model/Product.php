03413c6a4594b82f985677a6b18aee7267e4d5d5
vivek <vivekkumarrajput44@gmail.com>
2019-09-11 17:25:22 +0530

    create model and connect or make relationship

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,8 +3,11 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Review;
 
 class Product extends Model
 {
-    //
+    function reviews(){
+    	return $this->hasMany(Review::class);
+    }
 }
