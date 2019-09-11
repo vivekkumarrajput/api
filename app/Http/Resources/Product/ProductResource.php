3f0f5f0f64c9b112ed51f1b560f9a93b0ff3f2f4
vivek <vivekkumarrajput44@gmail.com>
2019-09-11 19:30:27 +0530

    products api created

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,8 +19,30 @@ class ProductResource extends JsonResource
           'Name' => $this->name,
           'Description' => $this->details,
           'Price' => $this->price,
-          'Stock' => $this->stock,
-          'Discount' => $this->discount
+          'Stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
+          'Discount' => $this->discount,
+          'TotalPrice' => round((1-($this->discount/100))*$this->price,2),
+
+          /* 
+             totalprice is =>
+                     17/100 = .17
+                     1-.17 = .83
+                     .83*price
+
+           */
+
+
+          // 'Rating' =>$this->reviews->sum('star')/$this->reviews->count(),
+          // round for if divide by zero then it give undefined error so use round
+
+          //if product don't any review then it will divided by zero then undefined it will gives error
+
+          'Rating' =>$this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No Review Yet',
+
+          'href' => [
+
+                    'review'=>route('reviews.index',$this->id)
+                 ],
 
         ];
     }
--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/products','ProductController');
+
 Route::group(['prefix => products'],function(){
    Route::apiResource('/{product}/reviews','ReviewController');
 });
